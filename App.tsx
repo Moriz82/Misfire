0f84163d440c1faa44fb5cc7f2838fabd0475233
Moriz <72836413+Moriz82@users.noreply.github.com>
2023-03-21 13:32:09 -0500

    init

--- App.tsx
+++ App.tsx
@@ -1,21 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useEffect, useRef, useState} from 'react';
+import {AppState} from 'react-native';
+import {extendTheme, NativeBaseProvider} from 'native-base';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './Screens/HomeScreen/HomeScreen';
 
-import React from 'react';
-import { SafeAreaView, Text, View } from "react-native";
+export default function App(): JSX.Element {
+  let dataFetched = false;
+  const appState = useRef(AppState.currentState);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
-function App(): JSX.Element {
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      'change',
+      async nextAppState => {
+        if (
+          appState.current.match(/inactive|background/) &&
+          nextAppState === 'active'
+        ) {
+          if (!dataFetched) {
+            //await fetchData();
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            dataFetched = true;
+          } else {
+            //await storeData();
+          }
+        }
+
+        appState.current = nextAppState;
+        setAppStateVisible(appState.current);
+        if (!dataFetched) {
+          //await fetchData();
+          dataFetched = true;
+        } else {
+          //await storeData();
+        }
+      },
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const Stack = createNativeStackNavigator();
+
+  // const colorTheme = {
+  //   brand: {
+  //     backgroundGray: '#222222',
+  //     cricketGreen: '#005B04',
+  //   },
+  // };
+
+  const theme = extendTheme({
+    components: {
+      Text: {
+        baseStyle: {
+          fontFamily: 'Urbanist',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: 15,
+          textAlign: 'center',
+          lineHeight: 18,
+          color: 'white',
+        },
+      },
+      Button: {
+        baseStyle: {
+          backgroundColor: '#005B04',
+          borderRadius: 8,
+        },
+      },
+    },
+  });
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-    </SafeAreaView>
+    <NativeBaseProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{headerShown: false, title: ''}}
+          />
+          {/*<Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{headerShown: false, title: ''}}
+          />
+          <Stack.Screen
+            name="TeamSelection"
+            component={TeamSelection}
+            options={{headerShown: false, title: ''}}
+          />*/}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-export default App;
---
+++ components/CustomTextInput.tsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+/*import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';*/
+
+type Props = {
+  placeholderText: string;
+  value: string | undefined;
+  onChangeText: any;
+  iconName: string;
+  isPassword: boolean;
+};
+
+export function CustomTextInput(props: Props) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isPasswordSecure, setIsPasswordSecure] = useState(true);
+
+  return (
+    <View
+      style={{
+        borderRadius: 10,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}>
+      {/*<MaterialCommunityIcons
+        name={props.iconName}
+        color="gray"
+        size={25}
+        style={{paddingRight: 5}}
+      />*/}
+      <TextInput
+        style={{
+          flex: 1,
+          height: 50,
+          fontFamily: 'Urbanist',
+          color: 'black',
+        }}
+        placeholder={props.placeholderText}
+        defaultValue={props.value}
+        onChangeText={props.onChangeText}
+        placeholderTextColor={'gray'}
+        secureTextEntry={isPasswordSecure && props.isPassword}
+        keyboardType={props.isPassword ? 'default' : 'email-address'}
+        autoCapitalize={'none'}
+      />
+      {/*{props.isPassword ? (
+        <MaterialCommunityIcons
+          name={isPasswordSecure ? 'eye-off' : 'eye'}
+          size={28}
+          color="gray"
+          onPress={() => {
+            isPasswordSecure
+              ? setIsPasswordSecure(false)
+              : setIsPasswordSecure(true);
+          }}
+        />
+      ) : null}*/}
+    </View>
+  );
+}
+
+// <MaterialCommunityIcons style={styles.root} name="email" color="black" size={25} />
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: '#F7F8F9',
+    borderRadius: 8,
+    boxSizing: 'border-box',
+  },
+  placeholderText: {
+    color: 'rgb(255, 255, 255)',
+    fontSize: 15,
+    fontWeight: '700',
+  },
+});
