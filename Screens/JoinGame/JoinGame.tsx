3bd8cefccb9a0cb6b30fbcb763f54dffda8feeb3
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2023-03-27 13:03:46 -0500

    added an image bruh

--- Screens/JoinGame/JoinGame.tsx
+++ Screens/JoinGame/JoinGame.tsx
@@ -1,12 +1,22 @@
 import {Image, SafeAreaView} from 'react-native';
 import {Box, StatusBar, Text, View} from 'native-base';
 import React, {useState} from 'react';
+import joinGameStyles from './JoinGame.styles';
+import {ImageButton} from '../../components/ImageButton';
 
 
 const JoinGame = (props: {navigation: any}) => {
     return(
     <SafeAreaView>
         <Text>your mom</Text>
+
+
+        <View style={{width: '10%', height: '10%'}}>
+          <ImageButton
+            image={require('../../assets/images/MisfireLogo.png')}
+            onPress={() => props.navigation.navigate('HomeScreen')}
+          />
+        </View>
     </SafeAreaView>
     );
 }
