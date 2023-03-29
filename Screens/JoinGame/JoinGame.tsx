861b68c2b8cd9a41adf2a7fe2b441113a58be0c4
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2023-03-29 14:41:02 -0500

    Developments on Join Game

--- Screens/JoinGame/JoinGame.tsx
+++ Screens/JoinGame/JoinGame.tsx
@@ -4,11 +4,21 @@ import React from 'react';
 import {ImageButton} from '../../components/ImageButton';
 
 import homeScreenStyles from '../HomeScreen/HomeScreen.styles';
+import { color } from 'native-base/lib/typescript/theme/styled-system';
 
 const JoinGame = (props: {navigation: any}) => {
   return (
     <SafeAreaView style={homeScreenStyles.safeAreaViewStyle}>
       <View style={{}}>
+      <>
+       <View style={{
+       width: '100%', 
+       height: '13%', 
+       backgroundColor: '#434343', 
+       justifyContent: 'flex-end',
+       alignItems: 'flex-start',
+       }}>
+
         <ImageButton
           image={require('../../assets/images/backButton.png')}
           onPress={() => props.navigation.navigate('HomeScreen')}
@@ -17,6 +27,10 @@ const JoinGame = (props: {navigation: any}) => {
         />
       </View>
     </SafeAreaView>
+    
+      <View style={{backgroundColor: '#959696', flex:1}}>
+      </View>
+      </>
   );
 };
 
