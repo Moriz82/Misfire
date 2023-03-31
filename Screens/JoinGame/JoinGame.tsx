b840703f19edb883384b5b4927001a12f3cdad66
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2023-03-31 12:37:54 -0500

    Update JoinGame.tsx

--- Screens/JoinGame/JoinGame.tsx
+++ Screens/JoinGame/JoinGame.tsx
@@ -1,6 +1,8 @@
 import {SafeAreaView} from 'react-native';
 import {View} from 'native-base';
+import {View, Text} from 'native-base';
 import React from 'react';
+import {StyledButton, TextStroke} from '../../components/StyledButton';
 import {ImageButton} from '../../components/ImageButton';
 
 import homeScreenStyles from '../HomeScreen/HomeScreen.styles';
@@ -12,11 +14,16 @@ const JoinGame = (props: {navigation: any}) => {
       <View style={{}}>
       <>
        <View style={{
+    <>
+      <View style={{
        width: '100%', 
        height: '13%', 
        backgroundColor: '#434343', 
        justifyContent: 'flex-end',
        alignItems: 'flex-start',
+       justifyContent: 'flex-start',
+       alignItems: 'flex-end',
+       flexDirection: 'row'
        }}>
 
         <ImageButton
@@ -25,12 +32,25 @@ const JoinGame = (props: {navigation: any}) => {
           height={50}
           width={50}
         />
+
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={{}}>Join Game </Text>
+        </TextStroke>
       </View>
     </SafeAreaView>
     
       <View style={{backgroundColor: '#959696', flex:1}}>
+      <View style={{backgroundColor: '#605A58', flex:1, alignItems: 'center'}}>
+        <ImageButton
+          image={require('../../assets/images/MisfireLogoWithText.png')}
+          height={200}
+          width={200}
+        />
       </View>
       </>
+
+      
+    </>
   );
 };
 
