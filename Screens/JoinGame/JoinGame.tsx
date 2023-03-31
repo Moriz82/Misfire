c9dcd8a84665e3b66de715642fc3eb282ec673c8
Moriz <72836413+Moriz82@users.noreply.github.com>
2023-03-31 12:27:16 -0500

    fixed zanes bs

--- Screens/JoinGame/JoinGame.tsx
+++ Screens/JoinGame/JoinGame.tsx
@@ -4,21 +4,19 @@ import React from 'react';
 import {ImageButton} from '../../components/ImageButton';
 
 import homeScreenStyles from '../HomeScreen/HomeScreen.styles';
-import { color } from 'native-base/lib/typescript/theme/styled-system';
+import {color} from 'native-base/lib/typescript/theme/styled-system';
 
 const JoinGame = (props: {navigation: any}) => {
   return (
     <SafeAreaView style={homeScreenStyles.safeAreaViewStyle}>
-      <View style={{}}>
-      <>
-       <View style={{
-       width: '100%', 
-       height: '13%', 
-       backgroundColor: '#434343', 
-       justifyContent: 'flex-end',
-       alignItems: 'flex-start',
-       }}>
-
+      <View
+        style={{
+          width: '100%',
+          height: '13%',
+          backgroundColor: '#434343',
+          justifyContent: 'flex-end',
+          alignItems: 'flex-start',
+        }}>
         <ImageButton
           image={require('../../assets/images/backButton.png')}
           onPress={() => props.navigation.navigate('HomeScreen')}
@@ -27,10 +25,6 @@ const JoinGame = (props: {navigation: any}) => {
         />
       </View>
     </SafeAreaView>
-    
-      <View style={{backgroundColor: '#959696', flex:1}}>
-      </View>
-      </>
   );
 };
 
