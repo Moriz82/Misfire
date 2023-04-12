0a81289ac54f89d92c9d14cac5c323ff55b6815e
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2023-04-12 13:22:25 -0500

    SETTINGS SCREEN DONE

--- Screens/SettingsScreen/SettingsScreen.tsx
+++ Screens/SettingsScreen/SettingsScreen.tsx
@@ -1,16 +1,17 @@
 import {View} from 'react-native';
-import {Text} from 'native-base';
+import {Text, Slider} from 'native-base';
 import React from 'react';
 import {ImageButton} from '../../components/ImageButton';
-import {TextStroke} from '../../components/StyledButton';
+import {TextStroke, StyledButton} from '../../components/StyledButton';
 import settingsStyles from './SettingsScreen.styles';
+import {CustomCheckBox} from '../../components/CustomCheckBox';
 
 const SettingScreen = (props: {navigation: any}) => {
   return (
     <>
       <View
         style={{
-          paddingTop: 20,
+          paddingTop: 25,
           backgroundColor: '#434343',
           alignItems: 'flex-start',
           flexDirection: 'row',
@@ -28,7 +29,7 @@ const SettingScreen = (props: {navigation: any}) => {
             alignItems: 'center',
             flex: 1,
             paddingRight: 70,
-            paddingTop: 15,
+            paddingTop: 20,
           }}>
           <TextStroke stroke={3} color={'#000000'}>
             <Text style={settingsStyles.buttonText}>Settings</Text>
@@ -36,7 +37,108 @@ const SettingScreen = (props: {navigation: any}) => {
         </View>
       </View>
 
-      <View style={{backgroundColor: '#605A58', flex: 1}} />
+      <View style={{backgroundColor: '#605A58', height: '10%', paddingTop: 10}}>
+        <View style={{width: '100%'}}>
+          <CustomCheckBox
+            displayText='Profanity'
+            width={45}
+            height={45}
+            isDark={false}
+            image1={require('../../assets/images/checkImage.png')}
+            image2={require('../../assets/images/blankImage.png')}
+          />
+        </View>
+      </View>
+
+      <View style={{backgroundColor: '#605A58', height: '4%'}}></View>
+
+      <View style={{backgroundColor: '#605A58', height: '5%', alignItems: 'center'}}>
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={settingsStyles.buttonText}>Max Character Count</Text>
+        </TextStroke>
+      </View>
+
+      <View style={{backgroundColor: '#605A58', height: '5%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={settingsStyles.buttonText}>1</Text>
+        </TextStroke>
+
+        <Slider w="3/4" colorScheme='orange' maxW="300" defaultValue={70} minValue={0} maxValue={100} accessibilityLabel="hello world" step={10}>
+          <Slider.Track>
+            <Slider.FilledTrack />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider>
+
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={settingsStyles.buttonText}>500</Text>
+        </TextStroke>
+      </View>
+
+
+      <View style={{backgroundColor: '#605A58', height: '10%', alignItems: 'center', paddingTop: 45}}>
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={settingsStyles.buttonText}>Number of Rounds</Text>
+        </TextStroke>
+      </View>
+
+      <View style={{backgroundColor: '#605A58', height: '5%', alignItems: 'center', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={settingsStyles.buttonText}>1</Text>
+        </TextStroke>
+
+        <Slider w="3/4" colorScheme='orange' maxW="300" defaultValue={70} minValue={0} maxValue={100} accessibilityLabel="hello world" step={10}>
+          <Slider.Track>
+            <Slider.FilledTrack />
+          </Slider.Track>
+          <Slider.Thumb />
+        </Slider>
+
+        <TextStroke stroke={3} color={'#000000'}>
+          <Text style={settingsStyles.buttonText}>20</Text>
+        </TextStroke>
+      </View>
+
+      <View style={{backgroundColor: '#605A58', height: '10%', paddingTop: 10, width: '100%'}}>
+          <CustomCheckBox
+            displayText='Allow Pictures'
+            width={45}
+            height={45}
+            isDark={false}
+            image1={require('../../assets/images/checkImage.png')}
+            image2={require('../../assets/images/blankImage.png')}
+          />
+      </View>
+      <View style={{backgroundColor: '#605A58', height: '10%', paddingTop: 10, width: '100%'}}>
+          <CustomCheckBox
+            displayText='Allow Audio'
+            width={45}
+            height={45}
+            isDark={false}
+            image1={require('../../assets/images/checkImage.png')}
+            image2={require('../../assets/images/blankImage.png')}
+          />
+      </View>
+      <View style={{backgroundColor: '#605A58', height: '10%', paddingTop: 10, width: '100%'}}>
+          <CustomCheckBox
+            displayText='Allow Video'
+            width={45}
+            height={45}
+            isDark={false}
+            image1={require('../../assets/images/checkImage.png')}
+            image2={require('../../assets/images/blankImage.png')}
+          />
+      </View>
+
+      <View style={{backgroundColor: '#605A58', flex:1, paddingTop: 30, width: '100%', padding: 25}}>
+        <StyledButton
+          onPress={() => {
+            props.navigation.navigate('HomeScreen');
+          }}
+          buttonText={'Continue'}
+          buttonColor={false}
+        />
+      </View>
     </>
   );
 };
