0aa930ef6c7c819f9e4ac142a5974b2ea0e39be7
Moriz <72836413+Moriz82@users.noreply.github.com>
2023-04-18 12:49:39 -0500

    fixed zanes bs

--- components/ImageButton.tsx
+++ components/ImageButton.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-<<<<<<< HEAD
-import {Avatar, Button} from 'native-base';
-=======
 import {Button} from 'native-base';
 import CircleImage from './CircleImage';
->>>>>>> fe065ca3a3d28abb417b3b242e4a260df628818d
+import {Image} from 'react-native';
 
 type Props = {
   onPress: any;
@@ -20,16 +16,11 @@ export function ImageButton(props: Props) {
     <Button
       style={{backgroundColor: props.isDark ? '#434343' : '#605A58'}}
       onPress={props.onPress}>
-<<<<<<< HEAD
       {/* <Image
         style={{height: props.height, width: props.width}}
         source={props.image}
         key={props.key}
       /> */}
-      <Avatar source={props.image} size='2xl'></Avatar>
-    </Button>
-  );
-=======
       {getImage()}
     </Button>
   );
@@ -45,5 +36,4 @@ export function ImageButton(props: Props) {
       />
     );
   }
->>>>>>> fe065ca3a3d28abb417b3b242e4a260df628818d
 }
