5dfb02273cfd1c993980ad452b406726615a7208
ZaneSmith8 <112508789+ZaneSmith8@users.noreply.github.com>
2023-04-20 14:10:17 -0500

    Vote Screen

--- Screens/VoteScreen/VoteScreen.tsx
+++ Screens/VoteScreen/VoteScreen.tsx
@@ -1,18 +1,93 @@
 import {View, Text, Avatar} from 'native-base';
 import React, {useState} from 'react';
-import {SafeAreaView} from 'react-native';
 import {TextStroke} from '../../components/StyledButton';
 import {CustomTextInput} from '../../components/CustomTextInput';
-import {ImageButton} from '../../components/ImageButton';
-import voteStyles from '../MessageScreen/MessageScreen.styles';
 import {ReadyButton} from '../../components/ReadyButton';
+import {SafeAreaView} from 'react-native';
+import voteStyles from './VoteScreen.styles';
+import {StyledButton} from '../../components/StyledButton';
 
 const VoteScreen = (props: {navigation: any}) => {
-  const [usernameText, setUsernameText] = useState('');
 
   return (
     <>
-      
+      <SafeAreaView style={{backgroundColor: '#605A58', height: '100%'}}>
+        <View style={{alignItems: 'center', paddingTop: 12, width: '100%', backgroundColor: '#605A58'}}>
+          <TextStroke stroke={3} color={'#000000'}>
+            <Text
+              style={{
+                padding: 20,
+                paddingTop: 30,
+                color: 'white',
+                fontSize: 40,
+                textAlign: 'center',
+                lineHeight: 50
+              }}>
+              Vote On A Message!{' '}
+            </Text>
+          </TextStroke>
+        </View>
+
+        <View style={{padding: 3}}>
+          <View style={voteStyles.emailTextInput}>
+            <StyledButton
+              onPress={() => {
+                props.navigation.navigate('VoteScreen');
+              }}
+              buttonText={'Example Message'}
+              buttonColor={false}
+            />
+          </View>
+        </View>
+
+        <View style={{padding: 3}}>
+          <View style={voteStyles.emailTextInput}>
+            <StyledButton
+              onPress={() => {
+                props.navigation.navigate('VoteScreen');
+              }}
+              buttonText={'Example Message'}
+              buttonColor={false}
+            />
+          </View>
+        </View>
+
+        <View style={{padding: 3}}>
+          <View style={voteStyles.emailTextInput}>
+            <StyledButton
+              onPress={() => {
+                props.navigation.navigate('VoteScreen');
+              }}
+              buttonText={'Example Message'}
+              buttonColor={false}
+            />
+          </View>
+        </View>
+
+        <View style={{padding: 3}}>
+          <View style={voteStyles.emailTextInput}>
+            <StyledButton
+              onPress={() => {
+                props.navigation.navigate('VoteScreen');
+              }}
+              buttonText={'Example Message'}
+              buttonColor={true}
+            />
+          </View>
+        </View>
+
+        <View style={{padding: 3}}>
+          <View style={voteStyles.emailTextInput}>
+            <StyledButton
+              onPress={() => {
+                props.navigation.navigate('VoteScreen');
+              }}
+              buttonText={'Example Message'}
+              buttonColor={false}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
     </>
   );
 };
